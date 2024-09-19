1f5c0f524c17451be30f683271e9ff82f9dc0ba6
Lama <131875343+Lamaalbakri@users.noreply.github.com>
2024-09-19 13:46:47 +0300

    Add search for current & previous requests pages

--- src/components/RequestsTable/CurrentRequests.jsx
+++ src/components/RequestsTable/CurrentRequests.jsx
@@ -1,11 +1,41 @@
-import { currentRequests } from './dummyData';
+import React, { useState } from 'react';
 import RequestsTable from './RequestsTable';
+import { currentRequests } from './dummyData';
 
 function CurrentRequests() {
+  const [query, setQuery] = useState('');
+  const [filteredRequests, setFilteredRequests] = useState(currentRequests);
+  
+  const handleSearch = (e) => {
+    const searchQuery = e.target.value.toLowerCase();
+    const filtered = currentRequests.filter(
+      (request) =>
+        request.id.toString().includes(searchQuery) ||
+        request.manufacturerName.toLowerCase().includes(searchQuery)
+    );
+    setQuery(searchQuery);
+    setFilteredRequests(filtered);
+  };
 
-    return <RequestsTable data={currentRequests} title="Current Requests" isPrevious={false} />;
-
-
+  return (
+    <div className='RequestsTable'>
+        <div className="header-row">
+            <div className="title">Current Requests</div>
+            <div className="search-container">
+                <div className='search-label'>Search by Name / ID</div>
+                <input
+                type="search"
+                placeholder="Search by Name / ID"
+                value={query}
+                onChange={handleSearch}
+                className="input-with-icon"
+                />
+            </div>
+        </div>
+        
+        <RequestsTable data={filteredRequests} />
+    </div>
+  );
 }
 
 export default CurrentRequests
